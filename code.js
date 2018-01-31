7aa1d25ddebd70110dc9d4d44f14b22858969dd1
DavidS <sdave0921@gmail.com>
2018-01-31 16:40:33 -0700

    commit

--- code.js
+++ code.js
@@ -4,10 +4,28 @@
 function getStats(txt) {
 
     let nChars = txt.length;
+    let nWords = 0;
+    // creates array of words
+    let wordArray = (function findWords(text){
+        arrayofwords = text
+        // replace special characters with whitespace
+        .replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/g, ' ')
+        // replace new lines, new tabs etc with whitespace
+        .replace(/\r\n|\n|\t|\r/g, ' ')
+        // splits string in terms of whitespace
+        .split(' ')
+        // filters out spaces from array
+        .filter(function(array) {
+            return array != ''
+        });
+        return arrayofwords;
+    }(txt));
+    nWords = wordArray.length;
+    console.log(wordArray);
 
     return {
         nChars,
-        nWords: 22,
+        nWords,
         nLines: 10,
         nNonEmptyLines: 22,
         averageWordLength: 3.3,
