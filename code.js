c6abf050681c0100ea4effd49d191d24530a68d7
DavidS <sdave0921@gmail.com>
2018-02-01 12:11:02 -0700

    commit

--- code.js
+++ code.js
@@ -2,18 +2,34 @@
 // this is just a stub for a function you need to implement
 //
 function getStats(txt) {
-
-
     let nChars = txt.length;
     let nWords = 0;
-    // finds length of an array split per new line
+    // finds length of an array split per new line; counts number of lines
     let nLines = (function findLines(text){
-        return text.split(/\r\n|\n|\r/).length;
+        let numLines = 0;
+        if (text.length>0){
+            numLines = text.split(/\r\n|\n|\r/).length;
+        }
+        return numLines;
+    }(txt));
+    // finds maximum line length
+    let maxLineLength = (function findMaxLineLength(text){
+        let lineArray = text.split(/\r\n|\n|\r/);
+        let maxLength = lineArray[0].length;
+        console.log(lineArray);
+        for (i=0;i<(lineArray.length-1);i++){
+            for (j=(i+1);j<lineArray.length;j++){
+                if (lineArray[j].length>lineArray[i].length){
+                    maxLength = lineArray[j].length;
+                }
+            }
+        }
+        return maxLength;
     }(txt));
     // find number of non empty lines
     let nNonEmptyLines = (function findLines(text){
         let count = 0;
-        lineArray = text.split(/\r\n|\n|\r/);
+        let lineArray = text.split(/\r\n|\n|\r/);
         for (i=0;i<lineArray.length;i++){
             if (lineArray[i].replace(/\s/g,'').length){
                 count++;
@@ -45,7 +61,7 @@ function getStats(txt) {
         nLines,
         nNonEmptyLines,
         averageWordLength: 3.3,
-        maxLineLength: 33,
+        maxLineLength,
         palindromes: ["12321", "kayak", "mom"],
         longestWords: ["xxxxxxxxx", "123444444"],
         mostFrequentWords: ["hello(7)", "world(1)"]
